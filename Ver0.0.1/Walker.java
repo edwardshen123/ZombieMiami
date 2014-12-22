7351e54e2cba9fc183a16202dafca46cb8e3eaed
edwardshen123 <e@eshen.me>
2014-12-22 08:25:18 -0500

    added zombie classes

---
+++ Ver0.0.1/Walker.java
@@ -0,0 +1,8 @@
+public class Walker extends Zombie{
+    
+    public Walker() {
+	setHealth(200);
+	setType("Walker");
+    }
+
+}
--- Ver0.0.1/Zombie.java
+++ Ver0.0.1/Zombie.java
@@ -3,7 +3,25 @@ public class Zombie {
     private int health;
     private String type;
 
-    public String lunge(character player) {
-	
+    public String claw(Jacket player) {
+	int damage = 100;
+	player.setHealth(player.getHealth - damage);
+	return type + " lunge at " + player + " for " + damage + " damages";
+    }
+
+    public int getHealth() {
+	return health;
+    }
+    
+    public void setHealth(int newHealth) {
+	health = newHealth;
+    }
+
+    public void setType(String newType) {
+	type = newType;
+    }
+
+    public String toString() {
+	return type;
     }
 }
