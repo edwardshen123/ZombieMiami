cfe5d61157cba888e7e7a4b196498a114f0d61ec
edwardshen123 <e@eshen.me>
2014-12-22 08:40:31 -0500

    added mask

---
+++ Ver0.0.1/Mask.java
@@ -0,0 +1,18 @@
+public class Mask {
+    
+    private String type;
+    private String boostType;
+    private int boost;
+
+    public String getBoostLocation() {
+	return boostType;
+    }
+    
+    public int getBoost() {
+	return boost;
+    }
+
+    public String toString() {
+	return type;
+    }
+}
