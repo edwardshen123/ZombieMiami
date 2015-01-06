05ee323c73b399b4fc161cb1554ffc56d5c1fd2c
pooneetthaper <pooneetthaper@gmail.com>
2015-01-06 08:42:28 -0500

    tried to make the stats, ran into errors :(

--- Ver0.0.1/Weapon.java
+++ Ver0.0.1/Weapon.java
@@ -1,7 +1,8 @@
 public class Weapon{
     private int Damage;
-    private boolean Ranged;
-    private Object[][] weaponInfo;
+    private int Ranged;
+    private int[][] weaponInfo;
+    private String[] weaponNames;
     
 
     public Weapon(String name){
@@ -14,14 +15,14 @@ public class Weapon{
     }
 
     public int findWeapon(String name){
-	for (int i=0; i<weaponInfo.length; i++){
-	    if (weaponInfo[i][0].equals(name)) return i;
+	for (int i=0; i<weaponNames.length; i++){
+	    if (weaponNames[i][0].equals(name)) return i;
 	}
 	return -1;
     }
 
     public void setDamage(int i){
-	Damage=weaponInfo[i][1];
+	Damage=weaponInfo[i][0];
     }
 
     public int getDamage(){
@@ -29,29 +30,45 @@ public class Weapon{
     }
 
     public void setRanged(int i){
-	Ranged=weaponInfo[i][2];
+	Ranged=weaponInfo[i][1];
     }
 
-    public boolean getRanged(){
-	return Ranged;
+    public int getRanged(){
+        return Ranged;
     }
 
     public void setupInfo(){
+	weaponInfo= new int[9][2];
+	//info for weapon with corresponding index in weaponName
+	//{Damage (just a number), Ranged (0=false and 1=true)}
 	weaponInfo={
-	    {"airsoft", 3, true},
-	    {"bananabomb", 10, false},
-	    {"bat", 2, false},
-	    {"cat", 4, false},
-	    {"cannon", 5, true},
-	    {"HolyHandGrenade", 10000000000000000000, true},
-	    {"mortar", 7, true},
-	    {"shotgun", 5, true},
-	    {"teeshirtgun", 2, true},
-	}
+	    {3, 1},
+	    {10, 0},
+	    {2, 0},
+	    {4, 0},
+	    {5, 1},
+	    {7, 1},
+	    {5, 1},
+	    {2, 1},
+	    {10000000, 1}
+	};
+
+	weaponNames=new String[9];
+	weaponNames = {
+	    "airsoft",
+	     "bananabomb",
+	     "bat",
+	     "cat",
+	     "cannon",
+	     "mortar",
+	     "shotgun",
+	     "teeshirtgun",
+	     "TheHolyHandGrenadeOfAntioch"
+	};
     }
 
     public static void main(String[] args){
-	Weapon w= new Weapon("HolyHandGrenade");
+	Weapon w= new Weapon("TheHolyHandGrenadeOfAntioch");
 	System.out.println(getDamage());
 	System.out.println(getRanged());
     }
