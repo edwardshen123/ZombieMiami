2ad72f7684278a1dc0206cf1dc10eb5ebf81f658
pooneetthaper <pooneetthaper@gmail.com>
2015-01-06 08:00:28 -0500

    Worked on the weapons stuff

--- Ver0.0.1/Weapon.java
+++ Ver0.0.1/Weapon.java
@@ -1,5 +1,58 @@
-public class Weapon(){
-    private Object[][];
-
+public class Weapon{
+    private int Damage;
+    private boolean Ranged;
+    private Object[][] weaponInfo;
     
+
+    public Weapon(String name){
+	setupInfo();
+	i=findWeapon(name);
+	if (i>0){
+	    setDamage(i);
+	    setRanged(i);
+	}
+    }
+
+    public int findWeapon(String name){
+	for (int i=0; i<weaponInfo.length; i++){
+	    if (weaponInfo[i][0].equals(name)) return i;
+	}
+	return -1;
+    }
+
+    public void setDamage(int i){
+	Damage=weaponInfo[i][1];
+    }
+
+    public int getDamage(){
+	return Damage;
+    }
+
+    public void setRanged(int i){
+	Ranged=weaponInfo[i][2];
+    }
+
+    public boolean getRanged(){
+	return Ranged;
+    }
+
+    public void setupInfo(){
+	weaponInfo={
+	    {"airsoft", 3, true},
+	    {"bananabomb", 10, false},
+	    {"bat", 2, false},
+	    {"cat", 4, false},
+	    {"cannon", 5, true},
+	    {"HolyHandGrenade", 10000000000000000000, true},
+	    {"mortar", 7, true},
+	    {"shotgun", 5, true},
+	    {"teeshirtgun", 2, true},
+	}
+    }
+
+    public static void main(String[] args){
+	Weapon w= new Weapon("HolyHandGrenade");
+	System.out.println(getDamage());
+	System.out.println(getRanged());
+    }
 }
