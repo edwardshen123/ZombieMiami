8ca4a2433b86eb27bd99aa231fdcad8e74841f55
pooneetthaper <pooneetthaper@gmail.com>
2015-01-07 08:41:05 -0500

    dealt with errors and finished Weapon code

--- Ver0.0.1/Weapon.java
+++ Ver0.0.1/Weapon.java
@@ -1,26 +1,33 @@
 public class Weapon{
-    private int Damage;
-    private int Ranged;
-    private int[][] weaponInfo;
-    private String[] weaponNames;
+    //private int Damage;
+    private boolean Ranged;
+    private boolean[] weaponInfo={true,false,false,false,true,
+				  false,true,false,true,true,
+				  true,true,true};
+    private String[] weaponNames={
+	"airsoft","bananabomb","bat","cat","cannon",
+	"golfclub","grenade","katana","mortar","POKEBALL",
+	"shotgun","teeshirtgun","TheHolyHandGrenadeOfAntioch"
+    };
     
 
-    public Weapon(String name){
-	setupInfo();
-	i=findWeapon(name);
-	if (i>0){
-	    setDamage(i);
-	    setRanged(i);
+    public Weapon(String n){
+	int a=findWeapon(n);
+	if (a>=0){
+	    //setDamage(a);
+	    setRanged(a);
 	}
+	else System.out.println("weapons not found");
     }
 
     public int findWeapon(String name){
 	for (int i=0; i<weaponNames.length; i++){
-	    if (weaponNames[i][0].equals(name)) return i;
+	    if (weaponNames[i].equals(name)) return i;
 	}
 	return -1;
     }
 
+    /*
     public void setDamage(int i){
 	Damage=weaponInfo[i][0];
     }
@@ -28,48 +35,19 @@ public class Weapon{
     public int getDamage(){
 	return Damage;
     }
+    */
 
     public void setRanged(int i){
-	Ranged=weaponInfo[i][1];
+	Ranged=weaponInfo[i];
     }
 
-    public int getRanged(){
+    public boolean getRanged(){
         return Ranged;
     }
 
-    public void setupInfo(){
-	weaponInfo= new int[9][2];
-	//info for weapon with corresponding index in weaponName
-	//{Damage (just a number), Ranged (0=false and 1=true)}
-	weaponInfo={
-	    {3, 1},
-	    {10, 0},
-	    {2, 0},
-	    {4, 0},
-	    {5, 1},
-	    {7, 1},
-	    {5, 1},
-	    {2, 1},
-	    {10000000, 1}
-	};
-
-	weaponNames=new String[9];
-	weaponNames = {
-	    "airsoft",
-	     "bananabomb",
-	     "bat",
-	     "cat",
-	     "cannon",
-	     "mortar",
-	     "shotgun",
-	     "teeshirtgun",
-	     "TheHolyHandGrenadeOfAntioch"
-	};
-    }
-
     public static void main(String[] args){
-	Weapon w= new Weapon("TheHolyHandGrenadeOfAntioch");
-	System.out.println(getDamage());
-	System.out.println(getRanged());
+	Weapon w= new Weapon("bananabomb");
+	//System.out.println(w.getDamage());
+	System.out.println(w.getRanged());
     }
 }
