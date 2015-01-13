aded03c0f077fd45a59d8c0a131d3c0d9e2ee561
pooneetthaper <pooneetthaper@gmail.com>
2015-01-13 08:42:58 -0500

    Layout worked on

--- Ver0.0.1/Layout.java
+++ Ver0.0.1/Layout.java
@@ -1,16 +1,19 @@
 public class Layout{
 
     public boolean[][] Walls;
-    public int[][]doorways=
-    {{;
 
 
     public Layout(){
-	Walls=new boolean[81][81];
+	Walls=new boolean[41][41];
 	for (int i=0; i< Walls.length;i++){
 	    for (int j=0; j< Walls[0].length;j++){
-		if (i%5==0 || j%5==0){
+		if (i%4==0 || j%4==0){
 		    Walls[i][j]=true;
+		}
+		if (i%4==2 || j%4==2){
+		    if (Math.floor(Math.random()*100)%2==0){
+			Walls[i][j]=false;
+		    }
 		}
 	    }
 	}
